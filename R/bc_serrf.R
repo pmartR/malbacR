a3917700929433a29d5412b81d91f5ba22ed0407
damon.leach@pnnl.gov <leac176@we45749.pnl.gov>
2023-05-09 07:33:37 -0700

    updated bc_serrf to match online code

--- R/bc_serrf.R
+++ R/bc_serrf.R
@@ -23,7 +23,6 @@
 #' pmart_amide <- group_designation(pmart_amide,main_effects = "group",batch_id = "batch")
 #' pmart_amide <- normalize_global(pmart_amide,subset_fn = "all",norm_fn = "median",
 #'                                apply_norm = TRUE,backtransform = TRUE)
-#' amide_combat <- bc_combat(omicsData = pmart_amide, use_groups = FALSE)
 #' impObj <- imputation(omicsData = pmart_amide)
 #' amide_imp <- apply_imputation(imputeData = impObj, omicsData = pmart_amide)
 #' amide_serrf <- bc_serrf(omicsData = amide_imp,sampletype_cname = "group",test_val = "QC")
@@ -38,6 +37,7 @@ bc_serrf <- function(omicsData, sampletype_cname, test_val){
   # check that omicsData is of appropriate class #
   if (!inherits(omicsData, c("pepData", "proData", "metabData", "lipidData",
                              "nmrData"))) {
+    amide_imp$f_data$
     
     stop (paste("omicsData must be of class 'pepData', 'proData', 'metabData',",
                 "'lipidData', or 'nmrData'",
@@ -123,7 +123,8 @@ bc_serrf <- function(omicsData, sampletype_cname, test_val){
   
   # obtain the batch information
   batch_info <- attributes(attr(omicsData,"group_DF"))$batch_id
-  batchName <- colnames(batch_info)[2]
+  batch_sampleID <- which(colnames(batch_info) == pmartR::get_fdata_cname(omicsData))
+  batchName <- colnames(batch_info)[-batch_sampleID]
   
   # list the columns of fdata to remove
   badfdata <- colnames(fdata)
@@ -136,6 +137,47 @@ bc_serrf <- function(omicsData, sampletype_cname, test_val){
   colnames(training_dat)[trainBatchNum] <- "batch"
   testBatchNum <- which(colnames(testing_dat) == batchName)
   colnames(testing_dat)[testBatchNum] <- "batch"
+  
+  # set up correlation matrix lists
+  corrs_train = list()
+  corrs_target = list()
+  
+  # find the correlation matrices for training and testing data
+  for(b in 1:length(unique(batch_info[[batchName]]))){
+    # find current batch
+    current_batch = unique(batch_info[[batchName]])[b]
+    
+    train_one_batch = subset(training_dat,training_dat[[batchName]] == current_batch)
+    test_one_batch = subset(testing_dat,testing_dat[[batchName]] == current_batch)
+    
+    # scale the training/QC data
+    # subset to only training data and transpose the data so we have molecule x sample
+    rownames(train_one_batch) <- NULL
+    train_one_batch <- tibble::column_to_rownames(train_one_batch,var = fdata_cname)
+    train_one_batch <- train_one_batch %>%
+      t() %>%
+      data.frame()
+    train_one_batch <- subset(train_one_batch,rownames(train_one_batch) %in% omicsData$e_data[[edata_cname]])
+    train_one_batch <- apply(train_one_batch,2,as.numeric)
+    
+    # do this for test data too
+    rownames(test_one_batch) <- NULL
+    test_one_batch <- tibble::column_to_rownames(test_one_batch,var = fdata_cname)
+    test_one_batch <- test_one_batch %>%
+      t() %>%
+      data.frame()
+    test_one_batch <- subset(test_one_batch,rownames(test_one_batch) %in% omicsData$e_data[[edata_cname]])
+    test_one_batch <- apply(test_one_batch,2,as.numeric)
+    
+    # scale the data (different for target and training it appears)
+    train_scale = t(apply(train_one_batch,1,scale))
+    test_scale = scale(test_one_batch)
+    
+    # find the correlation values for both target and training data (scaled)
+    # e_current_batch = all_scale
+    corrs_train[[b]] = cor(t(train_scale), method = "spearman")
+    corrs_target[[b]] = cor(t(test_scale), method = "spearman")
+  }
   
   # set up nested data so we can do analyses by batch
   # first we set up training data
@@ -179,6 +221,8 @@ bc_serrf <- function(omicsData, sampletype_cname, test_val){
       tst <- tst %>%
         tibble::column_to_rownames(var = fdata_cname)
       
+      
+      
       # create empty data matrix for new normalization methods
       tbl_colnames <- colnames(tst)
       norm_dat <- matrix(nrow=nrow(tst),ncol=ncol(tst))
@@ -187,6 +231,37 @@ bc_serrf <- function(omicsData, sampletype_cname, test_val){
       doParallel::registerDoParallel(parallel::detectCores()-1)
       # for each molecule normalize the data
       nd <- foreach::foreach(i = 1:ncol(norm_dat),.combine = cbind) %dopar% {
+        # find what variables we are working with num_cor
+        num_cor = 10
+        for(b in 1:length(unique(batch_info[[batchName]]))){
+          # find current batch
+          current_batch = unique(batch_info[[batchName]])[b]
+          
+          # select only the batch we are working with
+          corr_train_one_batch = corrs_train[[b]]
+          corr_test_one_batch = corrs_target[[b]]
+          
+          # order the correlation as it pertains to the molecule of interest j
+          corr_train_order = order(abs(corr_train_one_batch[,i]),decreasing = TRUE)
+          corr_test_order = order(abs(corr_test_one_batch[,i]),decreasing = TRUE)
+          
+          # initialize sel_var vector and set l to be default number to start with
+          sel_var = c()
+          l = num_cor
+          
+          # want to find the num (10) most correlated molecules to our current molecule
+          # of interest across both training and testing data
+          while(length(sel_var)<(num_cor)){
+            # find the intersection of the training and target correlation values
+            # until we find 10 that match (or other number if not 10 as default)
+            sel_var = intersect(corr_train_order[1:l], corr_test_order[1:l])
+            # we cannot use our own molecule
+            sel_var = sel_var[!sel_var == i]
+            # keep adding one to l until we get to 10 molecules that intersect
+            l = l+1
+          }
+        }
+          
         #i = 1
         # identify the current variable
         current_var = colnames(norm_dat)[i]
@@ -196,37 +271,46 @@ bc_serrf <- function(omicsData, sampletype_cname, test_val){
         # find the y response that molecule
         train_data_y = scale(trn[,trn_current_var_col],scale = F)
         # scale the QC training data
-        train_data_x = t(apply(trn[,-trn_current_var_col],1,scale))
+        train_data_x = apply(trn[,sel_var],2,scale)
         train_data = data.frame(train_data_y,train_data_x )
-        colnames(train_data) = c("y",colnames(norm_dat)[-trn_current_var_col])
+        colnames(train_data) = c("y",colnames(norm_dat)[sel_var])
         # scale the testing data
-        test_data = t(apply(tst,1,scale))
+        test_data = apply(tst[,sel_var],2,scale)
         test_data = data.frame(test_data)
         # ensure that column names are the molecules
-        colnames(test_data) = colnames(norm_dat)
-        colnames(test_data)[tst_current_var_col] <- "y"
+        colnames(test_data) = colnames(norm_dat)[sel_var]
+        #colnames(test_data)[tst_current_var_col] <- "y"
         
-        # run random forest
-        model = ranger::ranger(y~.,data= train_data)
+        # run random forest on this dataset
+        model = ranger::ranger(y~., data = train_data)
         
-        # current values for this batch and molecule
-        ii = as.numeric(unlist(tst[,trn_current_var_col]))
+        # begin adjusting the normalized values
+        # find the important values
+        # for QC samples
+        pred_bqc = predict(model,data = train_data)$predictions
+        abund_val_bqc = trn[,i]
+        abund_val_bqc_mean = mean(trn[,i],na.rm=T)
+        all_data_var_num_qc = which(colnames(training_dat) == colnames(trn)[i])
+        abund_val_qc_median = median(training_dat[,all_data_var_num_qc],na.rm=T)
+        # for nonQC samples
+        pred_bnq = predict(model, data = test_data)$predictions
+        abund_val_bnq = tst[,i]
+        abund_val_bnq_mean = mean(tst[,i],na.rm=T)
+        all_data_var_num_nq = which(colnames(training_dat) == colnames(trn)[i])
+        abund_val_nq_median = median(testing_dat[,all_data_var_num_nq],na.rm=T)
         
-        # systematic error
-        si = (predict(model,data = test_data)$predictions  + mean(ii,na.rm=TRUE))/(median(all_nonQC,na.rm = TRUE))
-        norm_val = ii/si
+        # now calculate serff adjsuted values
+        norm_vals_bqc = abund_val_bqc/((pred_bqc + abund_val_bqc_mean)/abund_val_qc_median)
+        norm_vals_bnq = abund_val_bnq/((pred_bnq + abund_val_bnq_mean)/abund_val_nq_median)
+        serrf_bqc = norm_vals_bqc/(median(norm_vals_bqc,na.rm=T)/abund_val_qc_median)
+        serrf_bnq = norm_vals_bnq/(median(norm_vals_bnq,na.rm=T)/abund_val_nq_median)
         
-        # multiply by iibar
-        iibar = median(all_nonQC,na.rm=TRUE)/median(norm_val,na.rm = TRUE)
-        norm_val = norm_val*iibar
-        
-        # add to the data frame of normalized values
-        norm_dat[,trn_current_var_col] <- norm_val
+        norm_dat[,trn_current_var_col] <- serrf_bnq
       }
       
       # now make it a datframe and add back in rownname information
       norm_dat <- data.frame(nd)
-      colnames(norm_dat) = colnames(norm_dat)
+      colnames(norm_dat) = colnames(tst)
       rownames(norm_dat) <- rownames(tst)
       return(norm_dat)
     }))
