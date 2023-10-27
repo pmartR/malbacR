54afd4153e0854e97ea707c52ca72f5b0c0ada0d
damon.leach@pnnl.gov <damon.leach@pnnl.gov>
2023-10-27 07:02:50 -0700

    updated scaling methods

--- R/scale_method.R
+++ R/scale_method.R
@@ -25,28 +25,28 @@ scale_method <- function (data, methods = c("auto", "range", "pareto", "vast",
   methods <- match.arg(methods)
   if (ncol(data) > 1) {
     switch(methods, auto = {
-      res <- apply(data, 1, function(x) (x - mean(x, na.rm = TRUE))/sd(x, 
+      res <- apply(data, 2, function(x) (x - mean(x, na.rm = TRUE))/sd(x, 
                                                                        na.rm = TRUE))
-      return(data.frame(t(res), check.names = FALSE))
+      return(data.frame(res, check.names = FALSE))
     }, range = {
-      res <- apply(data, 1, function(x) (x - mean(x, na.rm = TRUE))/(range(x,na.rm=T)[2] - 
+      res <- apply(data, 2, function(x) (x - mean(x, na.rm = TRUE))/(range(x,na.rm=T)[2] - 
                                                                        range(x,na.rm=T)[1]))
-      return(data.frame(t(res), check.names = FALSE))
+      return(data.frame(res, check.names = FALSE))
     }, pareto = {
-      res <- apply(data, 1, function(x) (x - mean(x, na.rm = TRUE))/sqrt(sd(x, 
+      res <- apply(data, 2, function(x) (x - mean(x, na.rm = TRUE))/sqrt(sd(x, 
                                                                             na.rm = TRUE)))
-      return(data.frame(t(res), check.names = FALSE))
+      return(data.frame(res, check.names = FALSE))
     }, vast = {
-      res <- apply(data, 1, function(x) mean(x, na.rm = TRUE) * 
+      res <- apply(data, 2, function(x) mean(x, na.rm = TRUE) * 
                      (x - mean(x, na.rm = TRUE))/(sd(x, na.rm = TRUE)^2))
-      return(data.frame(t(res), check.names = FALSE))
+      return(data.frame(res, check.names = FALSE))
     }, level = {
-      res <- apply(data, 1, function(x) (x - mean(x, na.rm = TRUE))/mean(x, 
+      res <- apply(data, 2, function(x) (x - mean(x, na.rm = TRUE))/mean(x, 
                                                                          na.rm = TRUE))
-      return(data.frame(t(res), check.names = FALSE))
+      return(data.frame(res, check.names = FALSE))
     }, power = {
-      res <- apply(data, 1, function(x) sqrt(x) - mean(sqrt(x),na.rm=T))
-      return(data.frame(t(res), check.names = FALSE))
+      res <- apply(data, 2, function(x) sqrt(x) - mean(sqrt(x),na.rm=T))
+      return(data.frame(res, check.names = FALSE))
     })
   }
 }
