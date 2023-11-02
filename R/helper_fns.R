ede00712da422e43ba7dbd3403d6ab4b96e16f15
damon.leach@pnnl.gov <damon.leach@pnnl.gov>
2023-11-02 13:41:33 -0700

    updated helper functions with testing

--- R/helper_fns.R
+++ R/helper_fns.R
@@ -32,15 +32,15 @@ get_batch_parameters <- function (omicsData) {
 
 #' Fetch the Batch Effect Method Name
 #' 
-#' Retrieves the name of the batch effect
+#' Retrieves the values from the batch effect method name
 #' 
 #' @param omicsData An object of class pepData, proData, metabData, lipidData,
 #'                  or nmrData.
 #' 
-#' @return A character vector specifying the batch correction method used (NULL if no batch correction applied)
+#' @return A character vector with the function name for batch correction
 #' 
 #' @export
-#' @name get_batch_method
+#' @name get_batch_parameters
 #' 
 get_batch_method <- function (omicsData) {
   # Check class of omicsData.
@@ -60,4 +60,36 @@ get_batch_method <- function (omicsData) {
   
   # Extract and return the group_DF attribute.
   return (attributes(omicsData)$data_info$batch_info$bc_method)
+}
+
+#' Fetch the Logical Vector of if data has been batch corrected
+#' 
+#' Retrieves the TRUE/FALSE statement of if data has been batch corrected
+#' 
+#' @param omicsData An object of class pepData, proData, metabData, lipidData,
+#'                  or nmrData.
+#' 
+#' @return A logical value specifying if batch correction has been applied or not (TRUE FALSE if no batch correction applied)
+#' 
+#' @export
+#' @name get_batch_method
+#' 
+get_data_batch <- function (omicsData) {
+  # Check class of omicsData.
+  if (!inherits(omicsData, c("pepData",
+                             "proData",
+                             "metabData",
+                             "lipidData",
+                             "nmrData",
+                             "seqData"))) {
+    
+    # Lay down an error in the console.
+    stop (paste("omicsData must be of class 'pepData', 'proData',",
+                "'metabData', 'lipidData', 'nmrData', or 'seqData'",
+                sep = " "))
+    
+  }
+  
+  # Extract and return the group_DF attribute.
+  return (attributes(omicsData)$data_info$batch_info$is_bc)
 }
---
+++ tests/testthat/test_helper_fns.R
@@ -0,0 +1,63 @@
+context('test helper functions')
+
+test_that('helper functions pull from correct attributes',{
+  
+  load(system.file('testdata',
+                   'mini_udn.Rdata',
+                   package = 'malbacR'))
+  
+  # Construct a pepData object with the edata, fdata, and emeta data frames.
+  mdata <- pmartR::as.metabData(e_data = edata,
+                                f_data = fdata,
+                                e_meta = emeta,
+                                edata_cname = 'Metabolite',
+                                fdata_cname = 'SampleID',
+                                emeta_cname = 'Metabolite')
+  attributes(mdata)$data_info$data_scale_orig <- "log2"
+  attributes(mdata)$data_info$data_scale <- "log2"
+  
+  expect_identical(get_batch_method(mdata), attributes(mdata)$data_info$batch_info$bc_method)
+  expect_identical(get_batch_parameters(mdata), attributes(mdata)$data_info$batch_info$params)
+  expect_identical(get_data_batch(mdata),attributes(mdata)$data_info$batch_info$is_bc)
+  
+  # attempt some batch correction methods
+  mdata <- pmartR::group_designation(mdata,main_effects = "Age",batch_id = "BatchNum")
+  molfilt <- pmartR::molecule_filter(mdata,use_groups=TRUE)
+  mdata <- pmartR::applyFilt(molfilt,mdata)
+  impObj <- imputation(mdata)
+  mdata <- apply_imputation(impObj,mdata)
+  
+  # run batch correction
+  mdata_nomis <- bc_nomis(mdata,"IS","IS")
+  mdata_eigenMS <- bc_eigenMS(mdata)
+  mdata_pareto <- bc_pareto(mdata)
+  # qcrfsc requires data on abundance scale
+  mdata_abundance <- pmartR::edata_transform(mdata,"abundance")
+  mdata_qcrfsc <- bc_qcrfsc(mdata_abundance,"QC","QC.NIST","RunOrderOverall","Age")
+  # combat requires data to be normalized
+  mdata_norm <- pmartR::normalize_global(mdata,"all","median",apply_norm = TRUE,backtransform = TRUE)
+  mdata_qcrfsc <- bc_combat(mdata_norm)
+
+  # check bc method of batch corrected data
+  expect_identical(get_batch_method(mdata_nomis), attributes(mdata_nomis)$data_info$batch_info$bc_method)
+  expect_identical(get_batch_method(mdata_qcrfsc), attributes(mdata_qcrfsc)$data_info$batch_info$bc_method)
+  expect_identical(get_batch_method(mdata_qcrfsc), attributes(mdata_qcrfsc)$data_info$batch_info$bc_method)
+  expect_identical(get_batch_method(mdata_eigenMS), attributes(mdata_eigenMS)$data_info$batch_info$bc_method)
+  expect_identical(get_batch_method(mdata_pareto), attributes(mdata_pareto)$data_info$batch_info$bc_method)
+  
+  # check parameters of batch corrected data
+  expect_identical(get_batch_parameters(mdata_nomis), attributes(mdata_nomis)$data_info$batch_info$params)
+  expect_identical(get_batch_parameters(mdata_qcrfsc), attributes(mdata_qcrfsc)$data_info$batch_info$params)
+  expect_identical(get_batch_parameters(mdata_qcrfsc), attributes(mdata_qcrfsc)$data_info$batch_info$params)
+  expect_identical(get_batch_parameters(mdata_eigenMS), attributes(mdata_eigenMS)$data_info$batch_info$params)
+  expect_identical(get_batch_parameters(mdata_pareto), attributes(mdata_pareto)$data_info$batch_info$params)
+  
+  # check true/false if batch correction has been done
+  # check parameters of batch corrected data
+  expect_identical(get_data_batch(mdata_nomis), attributes(mdata_nomis)$data_info$batch_info$is_bc)
+  expect_identical(get_data_batch(mdata_qcrfsc), attributes(mdata_qcrfsc)$data_info$batch_info$is_bc)
+  expect_identical(get_data_batch(mdata_qcrfsc), attributes(mdata_qcrfsc)$data_info$batch_info$is_bc)
+  expect_identical(get_data_batch(mdata_eigenMS), attributes(mdata_eigenMS)$data_info$batch_info$is_bc)
+  expect_identical(get_data_batch(mdata_pareto), attributes(mdata_pareto)$data_info$batch_info$is_bc)
+})
+  
